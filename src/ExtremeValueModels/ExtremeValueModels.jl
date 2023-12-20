c7dcb92e6f5d6567e1381f40d9e0a0a66a4454fb
Marcel Kipping <kipping.marcel@web.de>
2023-12-20 12:06:15 +0100

    added UnivariateExtremeValueModels

--- src/ExtremeValueModels/ExtremeValueModels.jl
+++ src/ExtremeValueModels/ExtremeValueModels.jl
@@ -4,3 +4,10 @@
 Abstract type for extreme value models.
 """
 abstract type ExtremeValueModel end
+
+"""
+    UnivariateExtremeValueModel
+
+Abstract type for univariate extreme value models.
+"""
+abstract type UnivariateExtremeValueModel <: ExtremeValueModel end
