deb5d4964a5f0139415740c22b52116b3e266f0b
Marcel Kipping <kipping.marcel@web.de>
2023-12-20 12:05:07 +0100

    removed init_distribution and loglikelihood

--- src/ExtremeValueModels/ExtremeValueModels.jl
+++ src/ExtremeValueModels/ExtremeValueModels.jl
@@ -4,22 +4,3 @@
 Abstract type for extreme value models.
 """
 abstract type ExtremeValueModel end
-
-"""
-    init_distribution(model::ExtremeValueModel, θ::Vector{<:Real})
-Returns the distribution corresponding to the model at all evalueation sites for the
-for the parameter vector θ.
-"""
-function init_distribution end
-
-"""
-    loglikelihood(model::ExtremeValueModel, θ::Vector{<:Real})
-
-Returns the log-likelihood of the data associated with the model under the assumption
-of the parameter vector θ.
-"""
-function loglikelihood(model::ExtremeValueModel, θ::Vector{<:Real})
-    values = model.data
-    distribution = init_distribution(model, θ)
-    return sum(logpdf.(distribution, values))
-end
