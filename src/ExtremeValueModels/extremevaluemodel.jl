2da41c62558ed7c1fc7153d7910472ac499b5395
Marcel Kipping <kipping.marcel@web.de>
2024-02-08 11:14:58 +0100

    moved definition of RegressionExtremeValueModel to regressionextremevaluemodel.jl

--- src/ExtremeValueModels/extremevaluemodel.jl
+++ src/ExtremeValueModels/extremevaluemodel.jl
@@ -4,10 +4,3 @@
 Abstract type for extreme value models.
 """
 abstract type ExtremeValueModel end
-
-"""
-    RegressionExtremeValueModel
-
-Abstract type for regression extreme value models.
-"""
-abstract type RegressionExtremeValueModel <: ExtremeValueModel end
---
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -0,0 +1,6 @@
+"""
+    RegressionExtremeValueModel
+
+Abstract type for regression extreme value models.
+"""
+abstract type RegressionExtremeValueModel <: ExtremeValueModel end
