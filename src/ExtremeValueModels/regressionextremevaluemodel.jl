d665c658792e5dfac3337213b6d19a9e15731e33
Marcel Kipping <kipping.marcel@web.de>
2024-02-09 15:25:09 +0100

    included regressiongevmodel.jl in regressionextremevaluemodel.jl

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -4,3 +4,5 @@
 Abstract type for regression extreme value models.
 """
 abstract type RegressionExtremeValueModel <: ExtremeValueModel end
+
+include(joinpath("RegressionExtremeValueModels","regressiongevmodel.jl"))
