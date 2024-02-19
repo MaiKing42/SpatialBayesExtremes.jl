d94848c6012bed4e18c57077051f35ee129b43f1
Marcel Kipping <kipping.marcel@web.de>
2024-02-19 11:42:04 +0100

    implemented function getdistribution for UnivariateGPDModel

--- src/ExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
+++ src/ExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
@@ -3,3 +3,7 @@ struct UnivariateGPDModel <: UnivariateExtremeValueModel
     μ::Float64
     σ::Float64
 end
+
+function getdistribution(θ::NamedTuple{(:μ, :σ, :ξ)}{T}) where T<:Real
+    return GPD(θ.μ, θ.σ, θ.ξ)
+end
