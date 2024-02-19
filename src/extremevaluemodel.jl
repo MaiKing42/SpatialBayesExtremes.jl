670b2475de1f203e0d552e04db857beea3de4741
Marcel Kipping <kipping.marcel@web.de>
2024-02-19 11:16:05 +0100

    essential functions for ExtremeValueModel

--- src/extremevaluemodel.jl
+++ src/extremevaluemodel.jl
@@ -7,3 +7,12 @@ abstract type ExtremeValueModel end
 
 include(joinpath("ExtremeValueModels","univariateextremevaluemodel.jl"))
 include(joinpath("ExtremeValueModels","regressionextremevaluemodel.jl"))
+
+function getdistribution end
+
+function loglikelihood(model::ExtremeValueModel,θ)
+    distributions = getdistribution(θ)
+    return sum(logpdf.(distributions, model.data))
+end
+
+likelihood(model::ExtremeValueModel,θ) = exp(loglikelihood(model,θ))
