0caf2846fa4b7c156b4fcde1ab2c0d665de56333
Marcel Kipping <kipping.marcel@web.de>
2024-02-19 11:32:20 +0100

    implemented getdistribution for UnivariateGEVModel

--- src/ExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
+++ src/ExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
@@ -1,3 +1,8 @@
 struct UnivariateGEVModel <: UnivariateExtremeValueModel
     data::Vector{Float64}
 end
+
+
+function getdistribution(θ::NamedTuple{(:μ, :σ, :ξ)}{T}) where T<:Real
+    return GEV(θ.μ, θ.σ, θ.ξ)
+end
