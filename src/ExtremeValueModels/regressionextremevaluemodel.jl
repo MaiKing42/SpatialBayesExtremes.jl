9201026d2bba46436e1fc227d24c6ef85a8d73de
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 11:35:29 +0100

    added function getValues for RegressionExtremeValueModel

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -9,3 +9,5 @@ include(joinpath("RegressionExtremeValueModels","regressiongevmodel.jl"))
 include(joinpath("RegressionExtremeValueModels","regressiongpdmodel.jl"))
 
 getDistribution(model::RegressionExtremeValueModel, θ::NamedTuple{(:location, :scale, :shape), Tuple{Array{Float64, 1}, Array{Float64, 1}, Array{Float64, 1}}}) = generateDistribution.(model, evaluateParameters(model, θ)...)
+
+getValues(model::RegressionExrtemeValueModel) = model.data[:, model.values]
