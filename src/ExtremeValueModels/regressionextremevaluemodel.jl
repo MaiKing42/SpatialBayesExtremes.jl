6003a9ee8b66b41da5ccec80337746d9e0a356fe
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 13:03:18 +0100

    fixed typo

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -10,4 +10,4 @@ include(joinpath("RegressionExtremeValueModels","regressiongpdmodel.jl"))
 
 getDistribution(model::RegressionExtremeValueModel, θ::NamedTuple{(:location, :scale, :shape), Tuple{Array{Float64, 1}, Array{Float64, 1}, Array{Float64, 1}}}) = generateDistribution.(model, evaluateParameters(model, θ)...)
 
-getValues(model::RegressionExrtemeValueModel) = model.data[:, model.values]
+getValues(model::RegressionExtremeValueModel) = model.data[:, model.values]
