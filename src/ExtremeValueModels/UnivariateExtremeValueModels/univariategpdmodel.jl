89d5d619c5155c539b4de592b35e89e8ae9bf133
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 13:35:03 +0100

    corrected number of arguments for GeneralizedPareto

--- src/ExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
+++ src/ExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
@@ -2,6 +2,6 @@ struct UnivariateGPDModel <: UnivariateExtremeValueModel
     data::Vector{Float64}
 end
 
-function getdistribution(model::UnivariateGPDModel,θ::NamedTuple{(:μ, :σ, :ξ),Tuple{Float64,Float64,Float64}})
-    return GPD(θ.μ, θ.σ, θ.ξ)
+function getdistribution(model::UnivariateGPDModel,θ::NamedTuple{(:σ, :ξ),Tuple{Float64,Float64}})
+    return GeneralizedPareto(θ.σ, θ.ξ)
 end
