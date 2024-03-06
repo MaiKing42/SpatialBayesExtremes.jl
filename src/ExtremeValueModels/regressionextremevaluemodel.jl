b52bc5e18bf6e3c02059466351c3560aa6023c67
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 11:00:06 +0100

    implemented getDistribution for RegressionExtremeValueModel

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -7,3 +7,5 @@ abstract type RegressionExtremeValueModel <: ExtremeValueModel end
 
 include(joinpath("RegressionExtremeValueModels","regressiongevmodel.jl"))
 include(joinpath("RegressionExtremeValueModels","regressiongpdmodel.jl"))
+
+getDistribution(model::RegressionExtremeValueModel, θ::NamedTuple{(:location, :scale, :shape), Tuple{Array{Float64, 1}, Array{Float64, 1}, Array{Float64, 1}}}) = generateDistribution.(model, evaluateParameters(model, θ)...)
