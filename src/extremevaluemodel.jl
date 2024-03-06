111773e807e410a2484194fdc144cd24e2679c21
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 15:45:32 +0100

    changed order

--- src/extremevaluemodel.jl
+++ src/extremevaluemodel.jl
@@ -5,12 +5,12 @@ Abstract type for extreme value models.
 """
 abstract type ExtremeValueModel end
 
-include(joinpath("ExtremeValueModels","univariateextremevaluemodel.jl"))
-include(joinpath("ExtremeValueModels","regressionextremevaluemodel.jl"))
-
 function loglikelihood(model::ExtremeValueModel,θ)
     distributions = getdistribution(model,θ)
     return sum(logpdf.(distributions, model.data))
 end
 
 likelihood(model::ExtremeValueModel,θ) = exp(loglikelihood(model,θ))
+
+include(joinpath("ExtremeValueModels","univariateextremevaluemodel.jl"))
+include(joinpath("ExtremeValueModels","regressionextremevaluemodel.jl"))
