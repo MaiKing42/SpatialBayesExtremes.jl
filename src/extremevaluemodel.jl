00186731b8004acaa845469f97f287522b4656c2
Marcel Kipping <kipping.marcel@web.de>
2024-03-06 15:44:56 +0100

    deleted default declaration of getdistribution

--- src/extremevaluemodel.jl
+++ src/extremevaluemodel.jl
@@ -8,10 +8,8 @@ abstract type ExtremeValueModel end
 include(joinpath("ExtremeValueModels","univariateextremevaluemodel.jl"))
 include(joinpath("ExtremeValueModels","regressionextremevaluemodel.jl"))
 
-function getdistribution end
-
 function loglikelihood(model::ExtremeValueModel,θ)
-    distributions = getdistribution(θ)
+    distributions = getdistribution(model,θ)
     return sum(logpdf.(distributions, model.data))
 end
 
