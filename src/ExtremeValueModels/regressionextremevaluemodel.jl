29c491c2e4b82487b2c8b849db855829f92560cb
Marcel Kipping <kipping.marcel@web.de>
2024-03-08 13:17:12 +0100

    fixed argument declaration of getdistribution for RegressionExtremeValueModel

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -5,7 +5,7 @@ Abstract type for regression extreme value models.
 """
 abstract type RegressionExtremeValueModel <: ExtremeValueModel end
 
-getdistribution(model::RegressionExtremeValueModel, θ::NamedTuple{(:location, :scale, :shape), Tuple{Array{Float64, 1}, Array{Float64, 1}, Array{Float64, 1}}}) = generateDistribution.(model, evaluateParameters(model, θ)...)
+getdistribution(model::RegressionExtremeValueModel, θ::NamedTuple) = generateDistribution.(model, evaluateParameters(model, θ)...)
 
 include(joinpath("RegressionExtremeValueModels","regressiongevmodel.jl"))
 include(joinpath("RegressionExtremeValueModels","regressiongpdmodel.jl"))
