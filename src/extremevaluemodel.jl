283c934f249090f56ed801c3c61e91eee427fba9
Marcel Kipping <kipping.marcel@web.de>
2024-03-26 14:11:09 +0100

    specified types for composite type ExtremeValueModel

--- src/extremevaluemodel.jl
+++ src/extremevaluemodel.jl
@@ -1,9 +1,28 @@
 """
+T<:ModelType specifies the type of the Extreme Value model.
+"""
+abstract type ModelType end
+
+"""
+    Regression <: ModelType
+
+Type for regression models.
+"""
+abstract type Regression <: ModelType end
+
+"""
+    Univariate <: ModelType
+
+Type for univariate models.
+"""
+abstract type Univariate <: ModelType end
+
+"""
     ExtremeValueModel{T,D}
 
 Abstract type for extreme value models.
 """
-abstract type ExtremeValueModel{T,D} end
+abstract type ExtremeValueModel{T<:ModelType,D<:Distribution} end
 
 function loglikelihood(model::ExtremeValueModel,θ)
     distributions = get_distribution(model,θ)
