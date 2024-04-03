2bdcbc2a9e0b7fd9599662c303fba1f01bf968bf
Marcel Kipping <kipping.marcel@web.de>
2024-04-03 14:00:51 +0200

    export functions and data types

--- src/SpatialBayesExtremes.jl
+++ src/SpatialBayesExtremes.jl
@@ -2,6 +2,13 @@ module SpatialBayesExtremes
 
 using Distributions, DataFrames, StatsModels, ProgressMeter
 
+export 
+    ExtremeValueModel,
+    RegressionExtremeValueModel,
+    UnivariateExtremeValueModel,
+    loglikelihood,
+    likelihood
+
 include("utils.jl")
 include("extremevaluemodel.jl")
 end
