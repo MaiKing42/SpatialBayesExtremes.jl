4745accc034800b20e46e9d3e6466befdc76df37
Marcel Kipping <kipping.marcel@web.de>
2024-09-03 16:34:00 +0200

    added deivatives of models for RegressionGPDModel

--- src/ExtremeValueModels/RegressionExtremeValueModels/regressiongpdmodel.jl
+++ src/ExtremeValueModels/RegressionExtremeValueModels/regressiongpdmodel.jl
@@ -9,10 +9,22 @@ function evaluateScaleParameter(model::RegressionGPDModel, σ::Vector{Float64})
     return exp.(model.scaleMatrix * σ)
 end
 
+function evaluateJacobian_scaleModel(model::RegressionGPDModel, σ::Vector{Float64})
+    return evaluateScaleParameter(model, σ).*model.scaleMatrix
+end
+
 function evaluateShapeParameter(model::RegressionGPDModel, ξ::Vector{Float64})
     return model.shapeMatrix * ξ
 end
 
+function evaluateJacobian_shapeModel(model::RegressionGPDModel, ξ::Vector{Float64})
+    return model.shapeMatrix
+end
+
 function evaluateParameters(model::RegressionModel, θ::NamedTuple{(:σ, :ξ), Tuple{Vector{Float64}, Vector{Float64}}})
     return (σ = evaluateLocationParameter(model, θ.σ), ξ = evaluateScaleParameter(model, θ.ξ))
+end
+
+function evaluateJacobian_parameterModels(model::RegressionGPDModel, θ::NamedTuple{(:σ, :ξ), Tuple{Vector{Float64}, Vector{Float64}}})
+    return (σ = evaluateJacobian_scaleModel(model, θ.σ), ξ = evaluateJacobian_shapeModel(model, θ.ξ))
 end
