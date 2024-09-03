4d14defac9ccf1d5843feece8bf768f0a05f19fd
Marcel Kipping <kipping.marcel@web.de>
2024-09-03 17:20:39 +0200

    added function loglikelihood_derivative for RegressionModel

--- src/ExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/regressionextremevaluemodel.jl
@@ -9,5 +9,11 @@ const RegressionExtremeValueModel = ExtremeValueModel{Regression}
 """
 get_distribution(model::RegressionExtremeValueModel{D},θ::NamedTuple) where {D} = D.(model, evaluateParameters(model, θ)...)
 
+function loglikelihood_derivative(model::RegressionExtremeValueModel,θ::NamedTuple)
+    scores = score_function(model,evaluateParameters(model, θ))
+    jacobians = evaluateJacobian_parameterModels(model, θ)
+    return NamedTuple(k=>(scores[k]'*jacobians[k]) for k in keys(scores))
+end
+
 include(joinpath("RegressionExtremeValueModels","regressiongevmodel.jl"))
 include(joinpath("RegressionExtremeValueModels","regressiongpdmodel.jl"))
