d1d336f70051704d1d4731fe1ff6b46f2b7df3d6
Marcel Kipping <kipping.marcel@web.de>
2024-10-31 18:18:19 +0100

    moved score_function for Univariate Extreme Value model for GEV distribtion

--- src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
+++ src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
@@ -1,3 +1,5 @@
 struct UnivariateGEVModel <: UnivariateExtremeValueModel{GeneralizedExtremeValue}
     data::Vector{Float64}
 end
+
+score_function(model::UnivariateExtremeValueModel{GeneralizedExtremeValue},θ::NamedTuple) where {T} = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
--- src/ExtremeValueModels/extremevaluemodel.jl
+++ src/ExtremeValueModels/extremevaluemodel.jl
@@ -11,7 +11,5 @@ likelihood(model::ExtremeValueModel,θ::NamedTuple) = exp(loglikelihood(model,θ
 
 score_function(model::ExtremeValueModel,θ::NamedTuple) =  NamedTuple(k=>ForwardDiff.derivative(x -> sum(logpdf.(get_distributions(model,merge(θ, [k=>x])...),1.0)),v) for (k,v) in pairs(θ))
 
-score_function(model::UnivariateExtremeValueModel{GeneralizedExtremeValue},θ::NamedTuple) where {T} = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
-
 include(joinpath("DiscreteExtremeValueModels","discreteextremevaluemodel.jl"))
 include(joinpath("ContinuousExtremeValueModels","continuousextremevaluemodel.jl"))
