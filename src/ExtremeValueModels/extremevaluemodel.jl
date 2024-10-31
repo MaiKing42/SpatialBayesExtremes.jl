4e82d32dd00c1eb13eea49fb22d2e55a22e7a03b
Marcel Kipping <kipping.marcel@web.de>
2024-10-31 17:18:10 +0100

    bugfix

--- src/ExtremeValueModels/extremevaluemodel.jl
+++ src/ExtremeValueModels/extremevaluemodel.jl
@@ -27,7 +27,7 @@ likelihood(model::ExtremeValueModel,θ::NamedTuple) = exp(loglikelihood(model,θ
 
 score_function(model::ExtremeValueModel,θ::NamedTuple) =  NamedTuple(k=>ForwardDiff.derivative(x -> sum(logpdf.(get_distributions(model,merge(θ, [k=>x])...),1.0)),v) for (k,v) in pairs(θ))
 
-score_function(model::ExtremeValueModel{T,GeneralizedExtremeValue},θ::NamedTuple) where {T} = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
+score_function(model::UnivariateExtremeValueModel{GeneralizedExtremeValue},θ::NamedTuple) where {T} = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
 
 include(joinpath("DiscreteExtremeValueModel","univariateextremevaluemodel.jl"))
 include(joinpath("ContinuousExtremeValueModel","regressionextremevaluemodel.jl"))
