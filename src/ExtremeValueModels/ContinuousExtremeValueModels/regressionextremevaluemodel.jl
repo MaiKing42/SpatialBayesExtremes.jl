a4f2d54dc802d228304fb96067398071ada1aca6
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 14:49:09 +0100

    defined function get_distribution for RegressionExtremeValueModel

--- src/ExtremeValueModels/ContinuousExtremeValueModels/regressionextremevaluemodel.jl
+++ src/ExtremeValueModels/ContinuousExtremeValueModels/regressionextremevaluemodel.jl
@@ -8,7 +8,9 @@ abstract type RegressionExtremeValueModel{D<:UnivariateDistribution} <: Continuo
 """
     get_distribution(model::RegressionExtremeValueModel, θ::NamedTuple)
 """
-get_distribution(model::RegressionExtremeValueModel{D},θ::NamedTuple) where {D} = D.(evaluateParameters(model, θ)...)
+get_distribution(model::RegressionExtremeValueModel{D},θ::NamedTuple) = get_singlesite_distribution.(model, evaluateParameters(θ))
+
+get_distribution(model::RegressionExtremeValueModel{D},covariates::DataFrame,θ::NamedTuple) = get_singlesite_distribution.(model, evaluateParameters(covariates,θ))
 
 function loglikelihood_derivative(model::RegressionExtremeValueModel,θ::NamedTuple)
     scores = score_function(model,evaluateParameters(model, θ))
