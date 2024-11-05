2d3382f28d87dfb5f5791342e768a7904610c04f
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 11:52:13 +0100

    removed function score_function, declared function get_distribution

--- src/extremevaluemodel.jl
+++ src/extremevaluemodel.jl
@@ -5,11 +5,12 @@ Abstract type for extreme value models.
 """
 abstract type ExtremeValueModel end
 
-loglikelihood(model::ExtremeValueModel,θ::NamedTuple) = sum(logpdf.(get_distibutions(model,θ), model.data))
+function get_distribution(model::ExtremeValueModel,θ::NamedTuple) end
+
+loglikelihood(model::ExtremeValueModel,θ::NamedTuple) = sum(logpdf.(get_distibution(model,θ), model.data))
 
 likelihood(model::ExtremeValueModel,θ::NamedTuple) = exp(loglikelihood(model,θ))
 
-score_function(model::ExtremeValueModel,θ::NamedTuple) =  NamedTuple(k=>ForwardDiff.derivative(x -> sum(logpdf.(get_distributions(model,merge(θ, [k=>x])...),1.0)),v) for (k,v) in pairs(θ))
 
 include(joinpath("ExtremeValueModels","discreteextremevaluemodel.jl"))
 include(joinpath("ExtremeValueModels","continuousextremevaluemodel.jl"))
