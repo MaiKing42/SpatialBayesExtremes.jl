887491c001c97835db3e3f92a5a5b0e7e6f361c0
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 14:21:39 +0100

    declared function get_distribution with additional argument covariates for Continuous ExtremeValueModel

--- src/ExtremeValueModels/continuousextremevaluemodel.jl
+++ src/ExtremeValueModels/continuousextremevaluemodel.jl
@@ -6,4 +6,6 @@ Abstract type for continuous extreme value models.
 
 abstract type ContinuousExtremeValueModel <: ExtremeValueModel end
 
+function get_distribution(model::ContinuousExtremeValueModel, covariates::DataFrame, θ::NamedTuple) end
+
 include(joinpath("ContinuousExtremeValueModels","regressionextremevaluemodel.jl"))
