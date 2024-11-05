565989abd697a27218c044b918488dba71d1de1a
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 14:42:29 +0100

    removed definition of get_distribution from UnivariateExtremeValueModel

--- src/ExtremeValueModels/DiscreteExtremeValueModels/univariateextremevaluemodel.jl
+++ src/ExtremeValueModels/DiscreteExtremeValueModels/univariateextremevaluemodel.jl
@@ -5,11 +5,6 @@ Abstract type for univariate extreme value models.
 """
 abstract type UnivariateExtremeValueModel{D<:UnivariateDistribution} <: DiscreteExtremeValueModel end
 
-"""
-    get_distribution(model::UnivariateExtremeValueModel, θ::NamedTuple)
-"""
-get_distribution(model::UnivariateExtremeValueModel{D},θ::NamedTuple) where {D} = D.(θ...)
-
 loglikelihood_derivative(model::UnivariateExtremeValueModel,θ::NamedTuple) = map(sum, score_function(model,θ))
 
 include(joinpath("UnivariateExtremeValueModels","univariategevmodel.jl"))
