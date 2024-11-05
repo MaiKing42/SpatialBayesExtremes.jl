0908d76fe63f0842a1abc0c32e47cce335813770
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 14:39:13 +0100

    adapted UnivariateGEVModel to new architecture

--- src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
+++ src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategevmodel.jl
@@ -2,4 +2,10 @@ struct UnivariateGEVModel <: UnivariateExtremeValueModel{GeneralizedExtremeValue
     data::Vector{Float64}
 end
 
-score_function(model::UnivariateExtremeValueModel{GeneralizedExtremeValue},θ::NamedTuple) where {T} = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
+get_parameters(model::UnivariateGEVModel) = (:μ,:σ,:ξ)
+
+_get_distribution(model::UnivariateGEVModel; μ, σ, ξ) = GeneralizedExtremeValue(μ,σ,ξ)
+
+get_distribution(model::UnivariateGEVModel,θ::NamedTuple) = _get_distribution(model; θ...)
+
+#score_function(model::UnivariateGEVModel,θ::NamedTuple) = score_gev.(model.data,θ.μ,θ.σ,θ.ξ)
