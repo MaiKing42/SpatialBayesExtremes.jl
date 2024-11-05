baa2ec8df469ba2c8ae52ea349a52f80ec956971
Marcel Kipping <kipping.marcel@web.de>
2024-11-05 15:39:41 +0100

    adapted RegressionGPDModel to new architecture

--- src/ExtremeValueModels/ContinuousExtremeValueModels/RegressionExtremeValueModels/regressiongpdmodel.jl
+++ src/ExtremeValueModels/ContinuousExtremeValueModels/RegressionExtremeValueModels/regressiongpdmodel.jl
@@ -1,12 +1,19 @@
 struct RegressionGPDModel<:RegressionExtremeValueModel{GeneralizedPareto}
     data::Vector{Float64}
+    scaleFormula::FormulaTerm
+    shapeFormula::FormulaTerm
     scaleMatrix::Matrix{Float64}
     shapeMatrix::Matrix{Float64}
 end
 
+get_parameters(model::RegressionGPDModel) = (:σ, :ξ)
 
 function evaluateScaleParameter(model::RegressionGPDModel, σ::Vector{Float64})
     return exp.(model.scaleMatrix * σ)
+end
+
+function evaluateScaleParameter(model::RegressionGPDModel, covariates::DataFrame, σ::Vector{Float64})
+    return exp.(modelmatrix(model.scaleFormula.rhs, covariates) * σ)
 end
 
 function evaluateJacobian_scaleModel(model::RegressionGPDModel, σ::Vector{Float64})
@@ -17,14 +24,18 @@ function evaluateShapeParameter(model::RegressionGPDModel, ξ::Vector{Float64})
     return model.shapeMatrix * ξ
 end
 
+function evaluateShapeParameter(model::RegressionGPDModel, covariates::DataFrame, ξ::Vector{Float64})
+    return modelmatrix(model.shapeFormula.rhs, covariates) * ξ
+end
+
 function evaluateJacobian_shapeModel(model::RegressionGPDModel, ξ::Vector{Float64})
     return model.shapeMatrix
 end
 
-function evaluateParameters(model::RegressionModel, θ::NamedTuple{(:σ, :ξ), Tuple{Vector{Float64}, Vector{Float64}}})
-    return (σ = evaluateLocationParameter(model, θ.σ), ξ = evaluateScaleParameter(model, θ.ξ))
-end
+_evaluateParameters(model::RegressionGPDModel; σ, ξ) = (σ = evaluateScaleParameter(model, σ), ξ = evaluateShapeParameter(model, ξ))
 
-function evaluateJacobian_parameterModels(model::RegressionGPDModel, θ::NamedTuple{(:σ, :ξ), Tuple{Vector{Float64}, Vector{Float64}}})
-    return (σ = evaluateJacobian_scaleModel(model, θ.σ), ξ = evaluateJacobian_shapeModel(model, θ.ξ))
-end
+evaluateParameters(model::RegressionModel, θ::NamedTuple) = _evaluateParameters(model; θ...)
+
+_evaluateJacobian_parameterModels(model::RegressionGPDModel; σ, ξ) = (σ = evaluateJacobian_scaleModel(model, σ), ξ = evaluateJacobian_shapeModel(model, ξ))
+
+evaluateJacobian_parameterModels(model::RegressionGPDModel, θ::NamedTuple) = _evaluateJacobian_parameterModels(model; θ...)
