65342196a8c5774bbb1b367efb7f5f2ad83a5dbb
Marcel Kipping <kipping.marcel@web.de>
2024-11-21 18:05:58 +0100

    updated definition of score  function for gev distribution

--- src/utils.jl
+++ src/utils.jl
@@ -1,22 +1,24 @@
-function score_gev(data::Real,μ::Real,σ::Real,ξ::Real)
+function score_function(dist::GeneralizedExtremeValue,data::Float64)
     ϵ = 1e-15
-    σ = σ + ϵ
+    μ = dist.μ
+    σ = dist.σ + ϵ
+    ξ = dist.ξ
     z = (data - μ)/σ
     ξ_z = ξ*z
-    if ξ == 0
+    if dist.ξ == 0
         u = exp(-z)
         dξ_log_u = z^2/2
     elseif ξ_z>-1
         u = (1+ξ_z).^(-1/ξ)
-        dξ_log_u = 1/ξ*(1/ξ*log(1+ξ_z)-z/(1+ξ_z))
+        dξ_log_u = 1/dist.ξ*(1/ξ*log(1+ξ_z)-z/(1+ξ_z))
     else
-        G[1] = Inf
-        G[2] = Inf
-        G[3] = Inf
-        return G
+        ∂μ = Inf
+        ∂σ = Inf
+        ∂ξ = Inf
+        return (μ=∂μ,σ=∂σ,ξ=∂ξ)
     end
-    G[1] = (ξ+1-u)/(σ*(1+ξ_z))
-    G[2] = ((1-u)*z-1)/(σ*(1+ξ_z))
-    G[3] = (1-u)*dξ_log_u-z/(1+ξ_z)
-    return G
+    ∂μ = (ξ+1-u)/(σ*(1+ξ_z))
+    ∂σ = ((1-u)*z-1)/(σ*(1+ξ_z))
+    ∂ξ = (1-u)*dξ_log_u-z/(1+ξ_z)
+    return (μ=∂μ,σ=∂σ,ξ=∂ξ)
 end
