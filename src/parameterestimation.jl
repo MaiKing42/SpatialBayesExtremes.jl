baed5a51ea1d1709e70cea089848f24b9322f862
Marcel Kipping <kipping.marcel@web.de>
2025-01-09 13:02:28 +0100

    created default function fit

--- src/parameterestimation.jl
+++ src/parameterestimation.jl
@@ -1,3 +1,5 @@
+function fit(data, modeltype, method) end
+
 include(joinpath("ParameterEstimations","pwm.jl"))
 include(joinpath("ParameterEstimations","maximumlikelihood.jl"))
 include(joinpath("ParameterEstimations","bayesian.jl"))
