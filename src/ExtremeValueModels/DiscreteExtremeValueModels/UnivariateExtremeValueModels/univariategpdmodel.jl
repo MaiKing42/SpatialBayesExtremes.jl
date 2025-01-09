0dc261d5f5c6f443633090daf15f029c6020c115
Marcel Kipping <kipping.marcel@web.de>
2025-01-09 15:13:03 +0100

    implemented basic functions for gpd model

--- src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
+++ src/ExtremeValueModels/DiscreteExtremeValueModels/UnivariateExtremeValueModels/univariategpdmodel.jl
@@ -1,3 +1,21 @@
 struct UnivariateGPDModel <: UnivariateExtremeValueModel{GeneralizedPareto}
-    data::Vector{Float64}
+    data::Vector{<:Real}
 end
+
+get_parameters(model::UnivariateGPDModel) = (:σ,:ξ)
+
+evaluateScaleParameter(model::UnivariateGPDModel, σ::Float64) = exp(σ)
+
+evaluateDerivativeScaleParameter(model::UnivariateGPDModel, σ::Float64) = exp(σ)
+
+evaluateShapeParameter(model::UnivariateGPDModel, ξ::Float64) = ξ
+
+evaluateDerivativeShapeParameter(model::UnivariateGPDModel, ξ::Float64) = 1.0
+
+_evaluateParameters(model::UnivariateGPDModel; σ, ξ) = (σ =  evaluateScaleParameter(model, σ),ξ = evaluateShapeParameter(model, ξ))
+
+evaluateParameters(model::UnivariateGPDModel, θ::NamedTuple) = _evaluateParameters(model; θ...)
+
+_evaluateDerivativeParameters(model::UnivariateGPDModel; σ, ξ) = (σ =  evaluateDerivativeScaleParameter(model, σ),ξ = evaluateDerivativeShapeParameter(model, ξ))
+
+evaluateDerivativeParameters(model::UnivariateGPDModel, θ::NamedTuple) = _evaluateDerivativeParameters(model; θ...)
