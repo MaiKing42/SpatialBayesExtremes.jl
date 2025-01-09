1d0c2224811e6dfeaa6d2df822d9256dd898df11
Marcel Kipping <kipping.marcel@web.de>
2025-01-09 14:58:47 +0100

    added default function fit with initial parameters

--- src/parameterestimation.jl
+++ src/parameterestimation.jl
@@ -1,5 +1,7 @@
 function fit(model, estimator) end
 
+function fit(model, estimator, initial_parameters) end
+
 include(joinpath("ParameterEstimations","pwm.jl"))
 include(joinpath("ParameterEstimations","maximumlikelihood.jl"))
 include(joinpath("ParameterEstimations","bayesian.jl"))
