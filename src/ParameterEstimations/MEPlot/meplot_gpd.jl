19d2a24cecf92f55cb932e7bd028b74c8ff6837b
Marcel Kipping <kipping.marcel@web.de>
2025-02-14 15:01:06 +0100

    removed GLM prefix from function call

--- src/ParameterEstimations/MEPlot/meplot_gpd.jl
+++ src/ParameterEstimations/MEPlot/meplot_gpd.jl
@@ -5,7 +5,7 @@ fitgpd_meplot(data::DataFrame, column::Symbol) = fit(UnivariateGPDModel(data[:,c
 function fit(data::Vector{<:Real}, estimator::typeof(PWMEstimator))
     mean_excess_data = empirical_mean_excess_plot(data)
     dataframe = DataFrame(X = data, Y = mean_excess_data)
-    coeffs = GLM.coef(GLM.lm(@formula(Y ~ X), dataframe))
+    coeffs = coef(lm(@formula(Y ~ X), dataframe))
     ξ = coeffs[2]/(coeffs[2]+1)
     σ = coeffs[1]/(1-ξ)
 
