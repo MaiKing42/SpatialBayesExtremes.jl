0389ebd2aef7d181160a243d956cca43cec0824d
Marcel Kipping <kipping.marcel@web.de>
2025-02-25 17:25:04 +0100

    implemented getinitialvalue for univariate gev maximum likelihood estimator

---
+++ src/ParameterEstimations/MaximumLikelihood/ml_gev_univ.jl
@@ -0,0 +1,7 @@
+function getInitialValue(model::ExtremeValueModel, estimator::Type{MaximumLikelihoodEstimator})
+    initialGuess = fitgevPWM(model.data).θ̂
+    flatInitialGuess, unflatten = value_flatten(initialGuess)
+    objective(θ::NamedTuple) = sum(-min.(0, 1.0 + θ.ξ*(model.data - θ.μ)/exp(θ.σ) + exp(θ.λ)))
+    opt = optimize(objective ∘ unflatten,θ -> only(Zygote.gradient(objective ∘ unflatten, θ)), flatInitialGuess, BFGS(linesearch = LineSearches.BackTracking()))
+    return unflatten(opt.minimizer)
+end
