5d7ad8250332482b27cba8618b3e04f9bc3182cd
Marcel Kipping <kipping.marcel@web.de>
2025-02-25 13:32:33 +0100

    renamed folder meplot into memethods

--- src/ParameterEstimations/meplot.jl
+++ src/ParameterEstimations/meplot.jl
@@ -7,5 +7,5 @@ function empiricalMeanExcessPlot(data::Vector{T}) where {T<:Real}
     return [mean_excess(data,data[i]) for i in 1:n-1]
 end
 
-include(joinpath("MEPlot","meplot_gpd.jl"))
-include(joinpath("MEPlot","hill_gpd.jl"))
+include(joinpath("MEMethods","meplot_gpd.jl"))
+include(joinpath("MEMethods","hill_gpd.jl"))
