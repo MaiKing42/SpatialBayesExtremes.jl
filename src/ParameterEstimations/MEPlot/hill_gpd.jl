d982dc2985182827a3e00da26560fd425a97d58c
Marcel Kipping <kipping.marcel@web.de>
2025-02-25 12:36:26 +0100

    use meanexcess over smallest data value for determination of shape parameter

--- src/ParameterEstimations/MEPlot/hill_gpd.jl
+++ src/ParameterEstimations/MEPlot/hill_gpd.jl
@@ -3,11 +3,8 @@ fitgpdHill(data::Vector{<:Real}) = fit(UnivariateGPDModel(data), typeof(PWMEstim
 fitgpdHill(data::DataFrame, column::Symbol) = fit(UnivariateGPDModel(data[:,column]), typeof(PWMEstimator))
 
 function fit(model::UnivariateGPDModel, estimator::typeof(HillEstimator))
-    data = model.data
-    mean_excess_logdata = empiricalMeanExcessPlot(log.(data))
-    dataframe = DataFrame(X = data, Y = mean_excess_logdata)
-    coeffs = coef(lm(@formula(Y ~ 1), dataframe))
-    ξ = coeffs[1]
+    data = sort(model.data)
+    ξ = empiricalMeanExcess(log.(data),log(data[1]))
     objective = σ -> -loglikelihood(model, (σ=σ[1],ξ=ξ))
     derivative = σ -> [-loglikelihoodDerivative(model, (σ=σ[1],ξ=ξ)).logσ,]
     logσ = optimize(objective, derivative, [0.0,], GradientDescent()).minimizer[1]
