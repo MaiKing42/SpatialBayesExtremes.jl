53df2462123a909cc2e47f8bd44827ac163fed0a
Marcel Kipping <kipping.marcel@web.de>
2025-02-25 14:50:44 +0100

    elementary implementation of maximum likelihood estimator

--- src/ParameterEstimations/maximumlikelihood.jl
+++ src/ParameterEstimations/maximumlikelihood.jl
@@ -1,5 +1,18 @@
 function getInitialValues(model::ExtremeValueModel, estimator::Type{MaximumLikelihoodEstimator}) end
 
-function fit(model::ExtremeValueModel, initialvalues::NamedTuple, estimator::Type{MaximumLikelihoodEstimator}) end
+function fit(model::ExtremeValueModel, initialvalues::NamedTuple, estimator::Type{MaximumLikelihoodEstimator})
+    flatInitialValues, unflatten = flatten(initialvalues)
+
+    objective(θ) = -loglikelihood(model, unflatten(θ))
+
+    function derivative(θ)
+        derivativeTuple = -loglikelihoodDerivative(model, unflatten(θ))
+        return flatten(derivativeTuple)[1]
+    end
+
+    opt = optimize(objective, derivative, flatInitialValues, BFGS(linesearch = LineSearches.BackTracking()))
+
+    return MaximumLikelihoodEstimator(model, unflatten(opt.minimizer))
+end
 
 fit(model::ExtremeValueModel, estimator::Type{MaximumLikelihoodEstimator}) = fit(model, getInitialValues(model, estimator), estimator)
