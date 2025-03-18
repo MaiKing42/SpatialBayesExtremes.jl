6a77782a6c3d833d31cebbb9f2db73a014658e3d
Marcel Kipping <kipping.marcel@web.de>
2025-03-18 16:06:05 +0100

    bugfix

--- src/ParameterEstimations/MaximumLikelihood/ml_gpd_regr.jl
+++ src/ParameterEstimations/MaximumLikelihood/ml_gpd_regr.jl
@@ -1,10 +1,10 @@
-fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(SpatialGPDModel(data, covariates, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
+fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(RegressionGPDModel(data, covariates, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
 
-fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(SpatialGPDModel(data[:,column], data, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
+fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(RegressionGPDModel(data[:,column], data, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
 
-fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(SpatialGPDModel(data, covariates, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
+fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(RegressionGPDModel(data, covariates, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
 
-fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(SpatialGPDModel(data[:,column], data, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
+fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(RegressionGPDModel(data[:,column], data, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
 
 function getInitialValue(model::SpatialGPDModel, estimator::Type{MaximumLikelihoodEstimator})
     univariateEstimate = fit(UnivariateGPDModel(model.data), estimator)
