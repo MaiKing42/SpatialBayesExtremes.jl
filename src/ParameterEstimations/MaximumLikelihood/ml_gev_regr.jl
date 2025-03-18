d64ab7ec277015edfa8ec6dbe925e7e0fd33cefe
Marcel Kipping <kipping.marcel@web.de>
2025-03-18 14:16:08 +0100

    implemented getinitialvalue for regressionGEVModel

--- src/ParameterEstimations/MaximumLikelihood/ml_gev_regr.jl
+++ src/ParameterEstimations/MaximumLikelihood/ml_gev_regr.jl
@@ -1,1 +1,11 @@
-function getInitialValue(model::RegressionGEVModel, estimator::Type{MaximumLikelihoodEstimator}) end
+function getInitialValue(model::RegressionGEVModel, estimator::Type{MaximumLikelihoodEstimator})
+    univariateEstimate = fit(UnivariateGEVModel(model.data), estimator)
+    initialμ = zeros(size(model.locationMatrix)[1])
+    initialμ[findfirst(==("(Intercept)"),model.locationTerm.rhs)] = univariateEstimate.θ̂.μ
+    initialσ = zeros(size(model.scaleMatrix)[1])
+    initialσ[findfirst(==("(Intercept)"),model.scaleTerm.rhs)] = univariateEstimate.θ̂.σ
+    initialξ = zeros(size(model.shapeMatrix)[1])
+    initialξ[findfirst(==("(Intercept)"),model.shapeTerm.rhs)] = univariateEstimate.θ̂.ξ
+
+    return (μ = initialμ, σ = initialσ, ξ = initialξ)
+end
