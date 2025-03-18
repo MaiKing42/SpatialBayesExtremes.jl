0b4a68295fdb1894071d1d112973127c63f381ea
Marcel Kipping <kipping.marcel@web.de>
2025-03-18 15:46:15 +0100

    implementation of fitSpatialgpdML

--- src/ParameterEstimations/MaximumLikelihood/ml_gpd_regr.jl
+++ src/ParameterEstimations/MaximumLikelihood/ml_gpd_regr.jl
@@ -1,1 +1,17 @@
-function getInitialValue(model::RegressionGPDModel, estimator::Type{MaximumLikelihoodEstimator}) end
+fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(SpatialGPDModel(data, covariates, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
+
+fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm) = fit(SpatialGPDModel(data[:,column], data, scaleFormula, shapeFormula), MaximumLikelihoodEstimator)
+
+fitSpatialgpdML(data::Vector{<:Real}, covariates::DataFrame, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(SpatialGPDModel(data, covariates, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
+
+fitSpatialgpdML(data::DataFrame, column::Symbol, scaleFormula::FormulaTerm, shapeFormula::FormulaTerm, initialvalue::NamedTuple) = fit(SpatialGPDModel(data[:,column], data, scaleFormula, shapeFormula), initialvalue, MaximumLikelihoodEstimator)
+
+function getInitialValue(model::SpatialGPDModel, estimator::Type{MaximumLikelihoodEstimator})
+    univariateEstimate = fit(UnivariateGPDModel(model.data), estimator)
+    initialσ = zeros(size(model.scaleMatrix,1))
+    initialσ[findfirst(==("(Intercept)"),coefnames(model.scaleTerm.rhs))] = univariateEstimate.θ̂.σ
+    initialξ = zeros(size(model.shapeMatrix,1))
+    initialξ[findfirst(==("(Intercept)"),coefnames(model.shapeTerm.rhs))] = univariateEstimate.θ̂.ξ
+
+    return (σ = initialσ, ξ = initialξ)
+end
