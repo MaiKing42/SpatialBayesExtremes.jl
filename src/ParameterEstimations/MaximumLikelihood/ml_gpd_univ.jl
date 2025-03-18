b06de96f5266ba1f2dfcc75d06282ee93327d0bf
Marcel Kipping <kipping.marcel@web.de>
2025-03-18 17:11:21 +0100

    implementation of fit for gpd maximum likelihood

--- src/ParameterEstimations/MaximumLikelihood/ml_gpd_univ.jl
+++ src/ParameterEstimations/MaximumLikelihood/ml_gpd_univ.jl
@@ -1,1 +1,9 @@
-function getInitialValue(model::UnivariateGPDModel, estimator::Type{MaximumLikelihoodEstimator}) end
+fitgpdML(data::Vector{<:Real}, threshold::Real) = fit(UnivariateGPDModel(data, threshold), MaximumLikelihoodEstimator)
+
+fitgpdML(data::DataFrame, column::Symbol, threshold::Real) = fit(UnivariateGPDModel(data[:,column], threshold), MaximumLikelihoodEstimator)
+
+fitgpdML(data::Vector{<:Real}, threshold::Real, initialvalue::NamedTuple) = fit(UnivariateGPDModel(data, threshold), initialvalue, MaximumLikelihoodEstimator)
+
+fitgpdML(data::DataFrame, column::Symbol, threshold::Real, initialvalue::NamedTuple) = fit(UnivariateGPDModel(data[:,column], threshold), initialvalue, MaximumLikelihoodEstimator)
+
+getInitialValue(model::UnivariateGPDModel, estimator::Type{MaximumLikelihoodEstimator}) = fitgpdHill(model.data).θ̂
