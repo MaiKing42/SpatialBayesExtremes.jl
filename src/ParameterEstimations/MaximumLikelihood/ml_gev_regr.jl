9a62a27b78c558e136979b5080ac76d9d89fd3be
Marcel Kipping <kipping.marcel@web.de>
2025-03-18 15:14:02 +0100

    increased clarity

--- src/ParameterEstimations/MaximumLikelihood/ml_gev_regr.jl
+++ src/ParameterEstimations/MaximumLikelihood/ml_gev_regr.jl
@@ -1,10 +1,12 @@
+fitSpatialgevML(data::Vector{<:Real})
+
 function getInitialValue(model::RegressionGEVModel, estimator::Type{MaximumLikelihoodEstimator})
     univariateEstimate = fit(UnivariateGEVModel(model.data), estimator)
-    initialμ = zeros(size(model.locationMatrix)[1])
+    initialμ = zeros(size(model.locationMatrix,1))
     initialμ[findfirst(==("(Intercept)"),coefnames(model.locationTerm.rhs))] = univariateEstimate.θ̂.μ
-    initialσ = zeros(size(model.scaleMatrix)[1])
+    initialσ = zeros(size(model.scaleMatrix,1))
     initialσ[findfirst(==("(Intercept)"),coefnames(model.scaleTerm.rhs))] = univariateEstimate.θ̂.σ
-    initialξ = zeros(size(model.shapeMatrix)[1])
+    initialξ = zeros(size(model.shapeMatrix,1))
     initialξ[findfirst(==("(Intercept)"),coefnames(model.shapeTerm.rhs))] = univariateEstimate.θ̂.ξ
 
     return (μ = initialμ, σ = initialσ, ξ = initialξ)
