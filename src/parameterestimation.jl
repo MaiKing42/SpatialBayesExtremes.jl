ce7e66e3ed19708d2c106c362489435323b9b787
Marcel Kipping <kipping.marcel@web.de>
2025-12-30 18:10:39 +0100

    added error messages

--- src/parameterestimation.jl
+++ src/parameterestimation.jl
@@ -1,6 +1,10 @@
-function fit(model, estimator) end
+function fit(model, estimator)
+    error("fit not implemented for this ExtremeValueModel and estimator")
+end
 
-function fit(model, estimator, initial_parameters) end
+function fit(model, estimator, initial_parameters)
+    error("fit not implemented for this ExtremeValueModel and estimator")
+end
 
 include(joinpath("ParameterEstimations","pwm.jl"))
 include(joinpath("ParameterEstimations","meplot.jl"))
