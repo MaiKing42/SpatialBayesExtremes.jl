063439d66f6884c52d9ad201d74948c9667b934f
Marcel Kipping <kipping.marcel@web.de>
2026-01-02 16:53:33 +0100

    added docs for continuous extreme value mode

--- docs/src/continuous.md
+++ docs/src/continuous.md
@@ -1,1 +1,7 @@
 # [Continuous Extreme Value Models](@id continuous)
+
+Contrary to the discrete extreme value models, continuous extreme value models require covariates and thus allow estimation of extreme events for evaluation sites with yet unobserved data.
+
+```@docs
+SpatialBayesExtremes.ContinuousExtremeValueModel
+SpatialBayesExtremes.getDistribution(model::ContinuousExtremeValueModel, covariates::DataFrame, θ::NamedTuple)
--- src/ExtremeValueModels/continuousextremevaluemodel.jl
+++ src/ExtremeValueModels/continuousextremevaluemodel.jl
@@ -1,5 +1,14 @@
+"""
+    abstract type ContinuousExtremeValueModel <: ExtremeValueModel end
+
+An abstract type for continuous extreme value models.
+"""
 abstract type ContinuousExtremeValueModel <: ExtremeValueModel end
 
+"""
+    getDistribution(model::ContinuousExtremeValueModel, covariates::DataFrame, θ::NamedTuple)
+Returns the probability distribution associated with the model and parameters for an observation sites indicated by `covariates`.
+"""
 getDistribution(model::ContinuousExtremeValueModel, covariates::DataFrame, θ::NamedTuple) = _getDistribution(model, covariates, θ)
 
 include(joinpath("ContinuousExtremeValueModels","regressionextremevaluemodel.jl"))
